a2f99fcad3b027314e2cbf46d78a265dee7e700c
Lapin Dmytro <lapin_dm@yahoo.com>
2024-01-24 19:22:28 +0100

    first initializing commit

---
+++ src/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    
+    return(
+        <header className="flex justify-between w-full border border-b-1 border-zinc-600">
+            <h1>Script generator</h1>
+        </header>
+    );
+}
+
+export default Header;
